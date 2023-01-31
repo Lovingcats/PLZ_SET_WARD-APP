0d8d665e0433cbb092318847657b57cfbd34039d
조용제 <whehdrms6810@naver.com>
2023-01-31 23:42:54 +0900

    [ADD] 진 컨테이너

--- lib/screens/bottombar/champion.dart
+++ lib/screens/bottombar/champion.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+
 class Champion extends StatefulWidget {
   const Champion({super.key});
 
@@ -12,10 +13,42 @@ class _ChampionState extends State<Champion> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
-      body: Center(
-        child: Text("챔피언 페이지", style: TextStyle(fontSize: 30.sp, color: Colors.black),),
-      )
-    );
+        appBar: null,
+        body: Center(
+            child: Column(
+          children: [
+            Container(
+              height: 400.h,
+              width: 300.w,
+              margin: EdgeInsets.only(top: 30.h),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.all(Radius.circular(15.r))),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  SizedBox(
+                    width: 270.w,
+                    height: 200.h,
+                    child: Image.asset(
+                      "images/jin.jpg",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 50.h),
+                    child: Text(
+                      "대충 애니메이션 공부를 위한 글씨입니다",
+                      style: TextStyle(fontSize: 17.sp, color: Colors.black),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        )));
   }
 }
