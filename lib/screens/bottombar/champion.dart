1b9b186e8fd6886810bd5703dbc6dc08306f6812
조용제 <whehdrms6810@naver.com>
2023-01-31 23:50:26 +0900

    [UPDATE] 진 container

--- lib/screens/bottombar/champion.dart
+++ lib/screens/bottombar/champion.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:plz_set_ward_app/screens/detail_champion.dart';
 
 class Champion extends StatefulWidget {
   const Champion({super.key});
@@ -17,35 +17,46 @@ class _ChampionState extends State<Champion> {
         body: Center(
             child: Column(
           children: [
-            Container(
-              height: 400.h,
-              width: 300.w,
-              margin: EdgeInsets.only(top: 30.h),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.all(Radius.circular(15.r))),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 20.h,
+            Padding(
+              padding: EdgeInsets.only(top: 30.h),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const DetailChampion()));
+                },
+                child: Container(
+                  height: 400.h,
+                  width: 300.w,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.all(Radius.circular(15.r))),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      SizedBox(
+                        width: 270.w,
+                        height: 200.h,
+                        child: Image.asset(
+                          "images/jin.jpg",
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 50.h),
+                        child: Text(
+                          "대충 애니메이션 공부를 위한 글씨입니다",
+                          style:
+                              TextStyle(fontSize: 17.sp, color: Colors.black),
+                        ),
+                      )
+                    ],
                   ),
-                  SizedBox(
-                    width: 270.w,
-                    height: 200.h,
-                    child: Image.asset(
-                      "images/jin.jpg",
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 50.h),
-                    child: Text(
-                      "대충 애니메이션 공부를 위한 글씨입니다",
-                      style: TextStyle(fontSize: 17.sp, color: Colors.black),
-                    ),
-                  )
-                ],
+                ),
               ),
             )
           ],
