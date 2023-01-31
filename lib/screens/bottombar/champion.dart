f1e16dd89f5309ce5674406bd0f2a19cfc007080
조용제 <whehdrms6810@naver.com>
2023-02-01 01:31:33 +0900

    [ADD] flutter animations

--- lib/screens/bottombar/champion.dart
+++ lib/screens/bottombar/champion.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:plz_set_ward_app/screens/detail_champion.dart';
@@ -19,42 +20,40 @@ class _ChampionState extends State<Champion> {
           children: [
             Padding(
               padding: EdgeInsets.only(top: 30.h),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => const DetailChampion()));
-                },
-                child: Container(
-                  height: 400.h,
-                  width: 300.w,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.all(Radius.circular(15.r))),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 20.h,
-                      ),
-                      SizedBox(
-                        width: 270.w,
-                        height: 200.h,
-                        child: Image.asset(
-                          "images/jin.jpg",
-                          fit: BoxFit.fill,
+              child: OpenContainer(
+                transitionDuration: const Duration(seconds: 1),
+                openBuilder: (context, action) => const DetailChampion(),
+                closedBuilder: (context, VoidCallback openContainer) => InkWell(
+                  onTap: openContainer,
+                  child: Container(
+                    height: 400.h,
+                    width: 300.w,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(15.r))),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 20.h,
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 50.h),
-                        child: Text(
-                          "대충 애니메이션 공부를 위한 글씨입니다",
-                          style:
-                              TextStyle(fontSize: 17.sp, color: Colors.black),
+                        SizedBox(
+                          width: 270.w,
+                          height: 200.h,
+                          child: Image.asset(
+                            "images/jin.jpg",
+                            fit: BoxFit.fill,
+                          ),
                         ),
-                      )
-                    ],
+                        Padding(
+                          padding: EdgeInsets.only(top: 50.h),
+                          child: Text(
+                            "대충 애니메이션 공부를 위한 글씨입니다",
+                            style:
+                                TextStyle(fontSize: 17.sp, color: Colors.black),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
